2060235e1dd9d56a60c3311d140ea60d3dffc703
Kiprotich-Amos <kiprotichamoh11@gmail.com>
2023-12-13 11:38:21 +0300

    JavaScript test done

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-const name = "Joe";
-const height = 74;
+const name = "Susan";
+const height = 34;
 const message = `${name} is ${height} inches tall`;
+
+const test = "test passed ";
+console.log(test);
 
 module.exports = {name, height, message};
